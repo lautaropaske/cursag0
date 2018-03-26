9fe22dfd91d4aad472cfebe08f89fd158a3628e1
Agustin Bettati <bettatiagustin@gmail.com>
2018-03-25 22:45:49 -0300

    Added testing for persistence of a user

---
+++ src/test/java/UserPersistenceTest.java
@@ -0,0 +1,39 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Agustin Bettati
+ * @version 1.0
+ */
+public class UserPersistenceTest {
+
+    @Test
+    public void createAndDeleteTest() {
+
+        User user = new User(-1,"jorge@hotmail.com","jorge","lin");
+
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(user);
+        transaction.commit();
+
+        User createdUser = session.find(User.class, -1);
+
+        assertEquals(-1,createdUser.getId());
+        assertEquals("jorge@hotmail.com",createdUser.getMail());
+
+        Transaction newTransaction = session.beginTransaction();
+        session.delete(createdUser);
+        newTransaction.commit();
+
+        session.close();
+
+    }
+
+}

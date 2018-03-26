b488bf8890a442abb17f052c75699ef41c8a3896
Lautaro Paskevicius <lautaro.paskevicius@ing.austral.edu.ar>
2018-03-26 12:08:43 -0300

    Added auto-generation to ID

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,7 +4,7 @@ import org.hibernate.cfg.Configuration;
 
 public class main {
     public static void main(String[] args) {
-        User user = new User(3,"g","h","i");
+        User user = new User(4,"g","h","i");
 
         SessionFactory sf = new Configuration().configure().buildSessionFactory();
         Session session = sf.openSession();

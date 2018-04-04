48586b6010d460f65affe1f8e93a2911b67d229f
Agustin Bettati <bettatiagustin@gmail.com>
2018-04-04 20:09:11 -0300

    api responding correctly

---
+++ src/main/java/resources/HelloResource.java
@@ -0,0 +1,19 @@
+package resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author Agustin Bettati
+ * @version 1.0
+ */
+@Path("/hello")
+public class HelloResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMessage() {
+        return "Hello world!";
+    }
+}

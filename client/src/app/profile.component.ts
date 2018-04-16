ae0eef2d83083fee0725f7e2d540128e2bb86061
Agustin Bettati <bettatiagustin@gmail.com>
2018-04-15 21:39:59 -0300

    Added detail of a course

--- client/src/app/profile.component.ts
+++ client/src/app/profile.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
-import {UserService} from "./user.service";
+import {Course} from "./Course";
+import {CourseService} from "./course.service";
 
 @Component({
   selector: 'profile',
@@ -14,19 +15,51 @@ import {UserService} from "./user.service";
       <a class="nav-item nav-link" [routerLink]="" (click)="logout();">Logout</a>
     </nav>
     <div class="container mt-5">
-        <h4 >Currently logged in to {{name}} {{surname}} (id: {{id}})</h4>
+        <h4>Currently logged in to {{name}} {{surname}} (id: {{id}})</h4>
+        <p>List of your created courses (Por ahora listeo todos):</p>
+      
+        <div *ngFor="let course of createdCourses" class="card" style="width: 18rem;">
+          <div class="card-body">
+            <a [routerLink]="['/details', course.id]">
+              <h5 class="card-title">{{course.name}}</h5>
+            </a>
+            <h6 class="card-subtitle mb-2 text-muted">{{course.rating}}</h6>
+            <p class="card-text">{{course.description}}</p>
+            <p class="card-text">Publisher: {{course.publisher.surname}}</p>
+          </div>
+        </div>
+      
 
-        <button type="button" (click)="createCourse();" class="btn btn-primary">Add new course</button>
+        <button type="button" (click)="createCourse();" class="btn mt-5 btn-primary">Add new course</button>
     </div> 
   `
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
 
-  id = localStorage.getItem("id");
+
+  id: number;
   name = localStorage.getItem("name");
   surname = localStorage.getItem("surname");
+  createdCourses: Course[] = [];
 
-  constructor(private router: Router) {}
+
+  constructor(private router: Router,private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    this.id = +localStorage.getItem("id");
+    //TODO esto va pedir los cursos creados por el usuario
+    this.courseService.getAllCourses().subscribe(
+      courses => {
+        console.log("Courses where found successfully.");
+        console.log(courses);
+        this.createdCourses = courses;
+      },
+      err => {
+        console.log("Unable to get courses from database.");
+        this.createdCourses = null;
+      }
+    );
+  }
 
   createCourse(): void{
     this.router.navigate(['create_course']);

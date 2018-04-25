f22a642ec2573a7bf166a2d0bc8473b6739cb7b7
Agustin Bettati <bettatiagustin@gmail.com>
2018-04-25 12:28:30 -0300

    fixed navbar and external links

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -7,13 +7,28 @@ import {Course} from "../models/Course";
   selector: 'navbar',
   templateUrl : './navbar.component.html'
 })
-export class NavbarComponent{
+export class NavbarComponent implements OnInit{
+
+
+  loggedIn: boolean;
 
   constructor(private router: Router,private courseService: CourseService) {}
 
+
+  ngOnInit(): void {
+    if(localStorage.getItem('token') == 'cursago'){
+      this.loggedIn = true;
+    }
+    else {
+      this.loggedIn = false;
+    }
+  }
+
+
+
   logout():void{
     localStorage.clear();
-    this.router.navigate(['access']);
+    this.router.navigate(['home']);
   }
 
 }
